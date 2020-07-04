389c24c83333830e0a7c368104dfc3b5b8f6f318
marchicman <manlio.marchica@live.it>
2020-07-04 12:42:08 +0200

    Initial Commit

---
+++ src/SimpleAPI/Controllers/ValuesController.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace SimpleAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<ValuesController> _logger;
+
+        public ValuesController(ILogger<ValuesController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ValuesController()
+        {
+            
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<string>> Get()
+        {
+           return new string[] {"value1", "value2"};
+        }
+
+        [HttpGet("{id}")]        
+        public ActionResult<string> Get(int id)
+        {
+           return "Les Jackson";
+        }
+    }
+}
---
+++ test/SimpleAPI.Tests/UnitTest1.cs
@@ -0,0 +1,26 @@
+using System;
+using Xunit;
+using SimpleAPI.Controllers;
+
+namespace SimpleAPI.Tests
+{
+  
+
+    public class UnitTest1
+    {
+        ValuesController controller = new ValuesController();
+
+        [Fact]
+        public void GetReturnsCorrectName()
+        {
+            var returnValue = controller.Get(1);
+            Assert.Equal("Les Jackson", returnValue.Value);
+        }
+
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
